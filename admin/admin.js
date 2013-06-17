12361b14c8a69ad760f81ceab15d0f0ba7eb9de3
Christophe Richon <richon.c@gmail.com>
2013-06-17 12:39:38 +0200

    automatic file upload

--- admin/admin.js
+++ admin/admin.js
@@ -45,16 +45,18 @@ var AdminView = Backbone.View.extend({
           this.listenTo(Evts, 'add', this.addEvt);
           this.listenTo(Evts, 'reset', this.addEvts);
           $('#editor').wysihtml5();
+            //select a date picker
+          $.datepicker.setDefaults({ dateFormat: 'yy-mm-dd' });          
     },
 
     newEvents: function(){
 
 		var $inputs = $('#container :input');
 
-		var values = {};
-		$inputs.each(function() {
-	        values[this.name] = $(this).val();
-        });
+		//var values = {};
+		//$inputs.each(function() {
+			//values[this.name] = $(this).val();
+        //});
         
         var branch = "";
         // retrieve branch value
@@ -64,12 +66,13 @@ var AdminView = Backbone.View.extend({
         var newEvt = new Evt({
             id: null,
             title: $('#container :input[name="title"]').val(),
-            description:  $('#editor').innerHtml,
+            description:  $('#container :input[name="desc"]').val(),
             type:  $('#container :input[name="type"]').val(),
             place:  $('#container :input[name="place"]').val(),
-            //select a date picker
-            begin_date: 1369820521, 
-            end_date: 1369820521,
+            begin_date: $('#container :input[name="begin_date"]').val(),
+            end_date: $('#container :input[name="end_date"]').val(),
+            img_path: $('#container :input[name="img_path"]').val(),
+            pdf_path: $('#container :input[name="pdf_path"]').val(),
             branch: branch
         });
         
@@ -135,4 +138,36 @@ router = new DocsRouter();
 Backbone.history.start();
 
 Evts.fetch();
+Students.fetch();
+$('.datepicker').datepicker();
 
+// document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+$(document).ready(function() {
+    $('#UploadForm').on('submit', function(e) {
+        e.preventDefault();
+        // $('#SubmitButton').attr('disabled', ''); // disable upload button
+        //show uploading message
+        $("#output").html('<div style="padding:10px"><img src="images/ajax-loader.gif" alt="Please Wait"/> <span>Uploading...</span></div>');
+        $(this).ajaxSubmit({
+            target: '#output',
+            success:  afterSuccess //call function after success
+        });
+    });
+});
+
+function afterSuccess(responseText, statusText, xhr, $form)  {
+    // $('#UploadForm').resetForm();  // reset form
+    // $('#SubmitButton').removeAttr('disabled'); //enable submit button
+    var answer = JSON.parse(responseText);
+    $('input[name="img_path"]').val(answer["img_path"]);
+    $('input[name="pdf_path"]').val(answer["pdf_path"]);
+
+    // alert('status: ' + statusText + '\n\nresponseText: \n' + responseText + 
+    //     '\n\nThe output div should have already been updated with the responseText.'); 
+}
+
+$('input[type="file"]').change(function(){ $('#UploadForm').submit(); });
+
+
+// document.getElementById('').addEventListener('change', handleFileSelect, false);
